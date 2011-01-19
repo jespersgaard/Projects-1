e0c0fb994cfef7dc5c3fe2eae37497a65c6c9709
Mark Myers <marcusmyers@gmail.com>
2011-01-19 13:23:26 -0500

    Added debug info to processPerms.php

--- bin/processPerms.php
+++ bin/processPerms.php
@@ -1,14 +1,18 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT']."/projects/inc/ProjectsDb.php");
-
+include("FirePHP.class.php");
+$fb = FirePHP::getInstance(true);
 
 $db = new ProjectsDb();
 $id = $_POST['uid'];
 $pid = $_POST['pid'];
 
-if(array_key_exist('remove',$_POST)){
-	$db->setPerms($id,$pid,true);
-} else {
+$fb->log($_POST,"Posted Items");
+exit();
+
+if(!array_key_exist('remove',$_POST)){
 	$db->setPerms($id,$pid);
+} else {
+	$db->setPerms($id,$pid,true);
 }
 
