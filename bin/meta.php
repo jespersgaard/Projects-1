2223fd1f1e6f0b6a7c8132b7cbc892a896b1fdea
Mark Myers <marcusmyers@gmail.com>
2011-01-19 12:35:11 -0500

    Added stripslashes() to posted querystring in meta.php

--- bin/meta.php
+++ bin/meta.php
@@ -10,7 +10,7 @@ if(isset($_POST['queryString'])){
 	if(preg_match("/, /",$qString)){
 		list($throwAway,$qString) = preg_split("/, /",$qString);
 	}
-	$fb->log($qString,"Query String");
+	$fb->log(stripslashes($qString),"Query String");
 	$arrSuggest = $db->getSuggestions($qString, &$fb);
 	foreach($arrSuggest as $key => $value){
 		echo "<li onClick=\"fill('".$value."');\">".$value."</li>";
