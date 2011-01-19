e2637fb6820d1e5558db3b96c13bca323ec0ec59
Mark Myers <marcusmyers@gmail.com>
2011-01-19 13:02:41 -0500

    Removed debug info in processPerms

--- bin/processPerms.php
+++ bin/processPerms.php
@@ -5,16 +5,10 @@ require_once($_SERVER['DOCUMENT_ROOT']."/projects/inc/ProjectsDb.php");
 $db = new ProjectsDb();
 $id = $_POST['uid'];
 $pid = $_POST['pid']
-$fb->log($id,"User ID");
-$fb->log($pid,"Project ID");
-
-print_r($_POST);
 
 if(array_key_exist('remove',$_POST)){
-    $fb->log("Remove from project");
 	$db->setPerms($id,$pid,true);
 } else {
-    $fb->log("Add to project");
 	$db->setPerms($id,$pid);
 }
 
