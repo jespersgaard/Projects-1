5d884441c6134613c62307ed1b167268241b97e1
Mark Myers <marcusmyers@gmail.com>
2011-01-19 13:19:07 -0500

    fixed issue without trailing ; after post object

--- bin/processPerms.php
+++ bin/processPerms.php
@@ -4,7 +4,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/projects/inc/ProjectsDb.php");
 
 $db = new ProjectsDb();
 $id = $_POST['uid'];
-$pid = $_POST['pid']
+$pid = $_POST['pid'];
 
 if(array_key_exist('remove',$_POST)){
 	$db->setPerms($id,$pid,true);
